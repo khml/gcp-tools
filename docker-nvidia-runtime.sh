2fb0b8b6c594544c0683178b96566b1b2e2247b5
khmlpy <43922054+khml@users.noreply.github.com>
2019-08-17 17:24:30 +0900

    nvidia runtimer installer

---
+++ docker-nvidia-runtime.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+
+curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | apt-key add -
+
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+
+curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list |tee /etc/apt/sources.list.d/nvidia-container-runtime.list
+
+apt-get update
+
+apt-get install -y nvidia-container-runtime
+
+# restart docker
+systemctl restart docker.service 
+
+# confirm nvidia runtime is installed or not
+docker run -it --rm --gpus all ubuntu nvidia-smi
+
+gpasswd -a ${USER} docker
+
