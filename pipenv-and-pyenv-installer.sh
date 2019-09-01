6a1deaa00ed25c5d0b0855a48db8fbd22f7a00ec
khml <43922054+khml@users.noreply.github.com>
2019-09-01 11:00:09 +0900

    install pipenv

--- pipenv-and-pyenv-installer.sh
+++ pipenv-and-pyenv-installer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 
+pip3 install pipenv
+
 git clone git://github.com/yyuu/pyenv.git ~/.pyenv
 echo '# >> pyenv setting >>===============' >> ~/.bashrc
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
